247327a1f4486e4a0a9b48ed8228fcfedbb461a2
Trần Xuân Sơn <sowndev@gmail.com>
2023-05-24 18:54:25 +0700

    Add camera movement

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,8 +5,10 @@ using System;
 public class LevelManager : MonoBehaviour
 {
     [SerializeField]
-    public GameObject[] tilePrefabs;
+    private GameObject[] tilePrefabs;
 
+    [SerializeField]
+    private CameraMovement cameraMovement;
     public float TileSize
     {
         get { return tilePrefabs[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x; }
@@ -29,21 +31,26 @@ public class LevelManager : MonoBehaviour
 
         int mapX = mapData[0].ToCharArray().Length;
         int mapY = mapData.Length;
+
+        Vector3 maxTile = Vector3.zero;
         Vector3 worldStart = Camera.main.ScreenToWorldPoint(new Vector3(0,Screen.height));
         for (int y = 0; y < mapY; y++)
         {
             char[] newTiles = mapData[y].ToCharArray();
             for (int x = 0; x < mapX; x++)
             {
-                PlaceTile(newTiles[x].ToString(),x,y,worldStart);
+               maxTile = PlaceTile(newTiles[x].ToString(),x,y,worldStart);
             }
         }
+
+        cameraMovement.SetLimits(new Vector3(maxTile.x +TileSize, maxTile.y - TileSize));
     }
-    private void PlaceTile(string tileType, int x, int y, Vector3 worldStart)
+    private Vector3 PlaceTile(string tileType, int x, int y, Vector3 worldStart)
     {
         int tileIndex = int.Parse(tileType);
         GameObject newTile = Instantiate(tilePrefabs[tileIndex]);
         newTile.transform.position = new Vector3(worldStart.x +( TileSize * x), worldStart.y - (TileSize * y), 0);
+        return newTile.transform.position;
     }
     private string[] ReadLevelText()
     {
